66d74da81477164ade1db98325842f461eaa5a42
Scott Rowell <rowell.scott@gmail.com>
2018-04-06 14:25:42 -0400

    Added searchbar

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./Landing";
 import Search from "./Search";
 
 const App = () => (
   <BrowserRouter>
     <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/search" component={Search} />
-      </Switch>
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
     </div>
   </BrowserRouter>
 );
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "this is some sort of debug statemtn"
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>Svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
